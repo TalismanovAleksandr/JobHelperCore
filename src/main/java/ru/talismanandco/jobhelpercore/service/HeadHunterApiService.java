8af96872b708825182493353292ffc8d6db3ec7f
Талисманов Александр Юрьевич <Aleksandr.Talismanov@rosbank.ru>
2022-02-15 21:53:51 +0300

    1. added Vacancy as Generic Type for List

--- src/main/java/ru/talismanandco/jobhelpercore/service/HeadHunterApiService.java
+++ src/main/java/ru/talismanandco/jobhelpercore/service/HeadHunterApiService.java
@@ -1,12 +1,14 @@
 package ru.talismanandco.jobhelpercore.service;
 
+import ru.talismanandco.jobhelpercore.dto.Vacancy;
+
 import java.util.List;
 
 public class HeadHunterApiService {
 
 
     //TODO поменять сигнатуру метода с List<?> на List<Vacancy>
-    public List<?> getVacancies(String queryParam) {
+    public List<Vacancy> getVacancies(String queryParam) {
         //1. TODO интеграция с HH API https://github.com/hhru/api/blob/master/docs/vacancies.md#search GET /vacancies
 
         //TODO использовать apache http client (CloseableHttpClient)
