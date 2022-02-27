92a14da4880b72c46dc25fa3a5b0dd1bc58050cd
Талисманов Александр Юрьевич <Aleksandr.Talismanov@rosbank.ru>
2022-02-27 18:40:26 +0300

    1. added CORS configuration

--- src/main/java/ru/talismanandco/jobhelpercore/config/JobHelperConfiguration.java
+++ src/main/java/ru/talismanandco/jobhelpercore/config/JobHelperConfiguration.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class JobHelperConfiguration {
@@ -14,6 +16,16 @@ public class JobHelperConfiguration {
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
                 false
         );
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/sorted_skills/{vacancy_name}").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
